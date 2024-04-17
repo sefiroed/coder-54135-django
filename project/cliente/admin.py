87dfe758848cd32b8672d5fb5cc701e920b80391
sefiroed <sefiroed@gmail.com>
2024-04-17 09:46:28 -0300

    Change

--- project/cliente/admin.py
+++ project/cliente/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Pais)
+admin.site.register(models.Cliente)
+admin.site.register(models.Producto)
